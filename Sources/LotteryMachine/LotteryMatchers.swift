6dd082df73208d2032d0dc6b10f6e3af4bab153a
Yu-Sung Loyi Hsu <loyihsu@icloud.com>
2021-08-19 23:11:32 +0800

    Add functionality to manually input the answers for the matching.

--- Sources/LotteryMachine/LotteryMatchers.swift
+++ Sources/LotteryMachine/LotteryMatchers.swift
@@ -31,4 +31,29 @@ public class LotteryMatchers {
         print("//////////////////// \(power.result) ///////////////////")
     }
 
+    public static func manual539Results(answers: String, myNumbers: String) {
+        print("")
+        print("手動539對獎結果")
+        let fiveThreeNine = FiveThreeNine(answer: answers, mine: myNumbers)
+        print("你的號碼：\(myNumbers.toIntArray())")
+        print("//////////////////// \(fiveThreeNine.results) ////////////////////")
+    }
+
+    public static func manualLotteryResults(answers: String, special: String, myNumbers: String) {
+        print("")
+        print("手動大樂透對獎結果")
+        let lottery = BigLottery(answer: answers, spAnswer: Int(special)!, mine: myNumbers)
+        print("你的號碼：\(myNumbers.toIntArray())")
+        print("//////////////////// \(lottery.result) ////////////////////")
+    }
+
+    public static func manualPowerResults(answers: String, area: String, myNumbers: String, myArea: String) {
+        print("")
+        print("手動威力彩對獎結果")
+        let power = PowerLottery(answer: answers, spAnswer: Int(area)!, mine: myNumbers, spMine: Int(myArea)!)
+        print("你的第一區：\(myNumbers.toIntArray())")
+        print("你的第二區：\(myArea)")
+        print("//////////////////// \(power.result) ///////////////////")
+    }
+
 }
